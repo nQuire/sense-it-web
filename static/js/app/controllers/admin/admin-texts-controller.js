3c1b1d0e8fd60b5c98ce220e945e4b683f8f5b51
Eloy Villasclaras <evilfer@gmail.com>
2014-08-11 15:39:46 +0100

    Update

--- static/js/app/controllers/admin/admin-texts-controller.js
+++ static/js/app/controllers/admin/admin-texts-controller.js
@@ -3,7 +3,13 @@ angular.module('senseItWeb', null, null).controller('AdminTextsCtrl', function (
     $scope.itemList = [
         {id: 'headerSubtitle', label: 'Header subtitle', format: false},
         {id: 'nquireTeaser', label: 'Project list teaser', format: true},
-        {id: 'about', label: 'About', format: true}
+        {id: 'about', label: 'About', format: true},
+        {id: 'createSenseIt', label: 'Create: Sense It intro', format: true},
+        {id: 'createSenseItHelp', label: 'Create: Sense It help', format: true},
+        {id: 'createSpotIt', label: 'Create: Spot It intro', format: true},
+        {id: 'createSpotItHelp', label: 'Create: Spot It help', format: true},
+        {id: 'createWinIt', label: 'Create: Win It intro', format: true},
+        {id: 'createWinItHelp', label: 'Create: Win It help', format: true}
     ];
 
 });
