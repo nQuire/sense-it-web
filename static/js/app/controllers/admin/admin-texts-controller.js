0e164841dbe3bfddfdb5522460af51b7268356c6
Eloy Villasclaras <evilfer@gmail.com>
2014-08-11 15:21:37 +0100

    Update

--- static/js/app/controllers/admin/admin-texts-controller.js
+++ static/js/app/controllers/admin/admin-texts-controller.js
@@ -1,6 +1,8 @@
 angular.module('senseItWeb', null, null).controller('AdminTextsCtrl', function ($scope) {
 
     $scope.itemList = [
+        {id: 'headerSubtitle', label: 'Header subtitle', format: false},
+        {id: 'nquireTeaser', label: 'Project list teaser', format: true},
         {id: 'about', label: 'About', format: true}
     ];
 
