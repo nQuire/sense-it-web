9e08b305cc6548d1d5bf3161e80b003d48721a93
Nick Freear <nfreear@yahoo.co.uk>
2014-10-14 14:59:46 +0100

    Bug #4/ #5, Fix "youtubeVideo" filter Javascript

--- static/src/js/app/filters/youtube-video.js
+++ static/src/js/app/filters/youtube-video.js
@@ -1,5 +1,7 @@
 angular.module('senseItWeb', null, null).filter('youtubeVideo', ['$sce', function ($sce) {
-  return function (val) {
-    return $sce.trustAsResourceUrl('//www.youtube.com/embed/' + val);
+  return function (value) {
+    // Parse for a video ID.
+	var m = value.match(/v=([^&]+)/);
+    return $sce.trustAsResourceUrl('//www.youtube.com/embed/' + (m ? m[1] : value));
   };
 }]);
