5aa6be7c4a6bf438911973e839d8daa0ba2889dc
evilfer <evilfer@gmail.com>
2014-11-13 20:48:59 +0000

    Fixed presentation of HTML comments in RHS menu.

--- app/src/main/java/org/greengin/nquireit/logic/rating/CommentFeedResponse.java
+++ app/src/main/java/org/greengin/nquireit/logic/rating/CommentFeedResponse.java
@@ -2,11 +2,13 @@ package org.greengin.nquireit.logic.rating;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.apache.commons.lang3.text.WordUtils;
 import org.greengin.nquireit.entities.projects.Project;
 import org.greengin.nquireit.entities.rating.Comment;
 import org.greengin.nquireit.entities.rating.CommentThreadEntity;
 import org.greengin.nquireit.utils.StringUtils;
+import org.jsoup.Jsoup;
 
 /**
  * Created by evilfer on 10/29/14.
@@ -26,7 +28,7 @@ public class CommentFeedResponse {
 
     public CommentFeedResponse(Comment comment) {
         CommentThreadEntity thread = comment.getTarget();
-        this.comment = StringUtils.ellipsis(comment.getComment(), 20);
+        this.comment = StringUtils.ellipsis(Jsoup.parse(comment.getComment()).text(), 20);
         this.threadTitle = thread.getTitle();
         this.threadId = thread.getId();
     }
