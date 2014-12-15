1ba22aef97c3a233c229d08ebcd5261c08d7a5be
evilfer <evilfer@gmail.com>
2014-12-15 21:16:00 +0000

    Several bugfixes: - overlapping text in home. - "busy?" debug text in spotit image upload. - search in home.

--- app/src/main/java/org/greengin/nquireit/entities/projects/ProjectMetadataBlock.java
+++ app/src/main/java/org/greengin/nquireit/entities/projects/ProjectMetadataBlock.java
@@ -2,6 +2,7 @@ package org.greengin.nquireit.entities.projects;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
@@ -18,7 +19,7 @@ public class ProjectMetadataBlock {
     String content;
 
     public boolean containsKeyword(String keyword) {
-        return (title != null && title.contains(keyword)) ||
-                (content != null && content.contains(keyword));
+        return (title != null && StringUtils.containsIgnoreCase(title, keyword)) ||
+                (content != null && StringUtils.containsIgnoreCase(content, keyword));
     }
 }
